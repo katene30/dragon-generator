bbb5d13d998d8accafd1a40bc81c995e5aff3fc5
Katene Jack Cotton <katenejack@gmail.com>
2019-01-28 21:37:27 +1300

    fixed random button

--- seeds/dragons.js
+++ seeds/dragons.js
@@ -8,9 +8,9 @@ exports.seed = function(knex, Promise) {
         {id: 1, Gender: 'male', image: 'images/image1.jpg', firstName: 'Puff', lastName: 'the magic dragon'},
         {id: 2, Gender: 'male', image: 'images/image2.png', firstName: 'Toothless', lastName: 'the toothless'},
         {id: 3, Gender: 'male', image: 'images/image3.jpg', firstName: 'Smaug', lastName: 'destroyer of worlds'},
-        {id: 4, Gender: 'female', image: 'images/image4', firstName: 'Saphira', lastName: 'Brightscales'},
-        {id: 5, Gender: 'female', image: 'images/image5', firstName: 'Spyro', lastName: 'the purple dragon'},
-        {id: 6, Gender: 'female', image: 'images/image6', firstName: 'Dragon', lastName: 'the wife of a donkey'}
+        {id: 4, Gender: 'female', image: 'images/image4.jpg', firstName: 'Saphira', lastName: 'Brightscales'},
+        {id: 5, Gender: 'female', image: 'images/image5.png', firstName: 'Spyro', lastName: 'the purple dragon'},
+        {id: 6, Gender: 'female', image: 'images/image6.png', firstName: 'Dragon', lastName: 'the wife of a donkey'}
       ]);
     });
 };
