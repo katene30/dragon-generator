d3611f235020cc4f951767dec376c469237f1d54
Katene Jack Cotton <katenejack@gmail.com>
2019-01-28 23:48:46 +1300

    added two more dragons

--- seeds/dragons.js
+++ seeds/dragons.js
@@ -10,7 +10,10 @@ exports.seed = function(knex, Promise) {
         {id: 3, gender: 'male', image: 'images/image3.jpg', firstName: 'Smaug', lastName: 'destroyer of worlds'},
         {id: 4, gender: 'female', image: 'images/image4.jpg', firstName: 'Saphira', lastName: 'Brightscales'},
         {id: 5, gender: 'female', image: 'images/image5.png', firstName: 'Spyro', lastName: 'the purple dragon'},
-        {id: 6, gender: 'female', image: 'images/image6.png', firstName: 'Dragon', lastName: 'the wife of a donkey'}
+        {id: 6, gender: 'female', image: 'images/image6.png', firstName: 'Dragon', lastName: 'the wife of a donkey'},
+        {id: 7, gender: 'male', image: 'images/image7.png', firstName: 'Mushu', lastName: 'the guardian spirit'},
+        {id: 8, gender: 'female', image: 'images/image8.png', firstName: 'Maleficent', lastName: 'Guardian of the Moors'}
+
       ]);
     });
 };
